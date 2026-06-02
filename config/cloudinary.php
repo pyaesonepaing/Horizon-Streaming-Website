2ea4f316a815e4f468d10c03e5f7c3e63bd9f44a
gressil1 <yanxiaoxiao2005@gmail.com>
2026-06-02 11:03:21 +0630

    update smth

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -31,7 +31,8 @@ return [
     |
     |
     */
-    'cloud_url' => env('CLOUDINARY_URL', 'cloudinary://'.env('CLOUDINARY_KEY').':'.env('CLOUDINARY_SECRET').'@'.env('CLOUDINARY_CLOUD_NAME')),
+    'cloud_url' => env('CLOUDINARY_URL'),
+
 
     /**
      * Upload Preset From Cloudinary Dashboard
